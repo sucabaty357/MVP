179f7729fa1b4a32b504ee300260aef00ae4948d
Thinh Nguyen <thinhnguyen@thinhs-macbook-air.local>
2019-09-04 10:38:38 +0700

    Initial Commit

---
+++ MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Thinh Nguyen on 9/4/19.
+//  Copyright © 2019 Trong Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
